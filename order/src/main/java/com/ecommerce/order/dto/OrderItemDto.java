ac8e5a05eaf998cd6d0258ecb75a468a3bef5ee3
rio-aman <amprajapati08893.com>
2025-08-05 10:32:13 +0530

    updation in files

---
+++ order/src/main/java/com/ecommerce/order/dto/OrderItemDto.java
@@ -0,0 +1,16 @@
+package com.ecommerce.order.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+public class OrderItemDto {
+    private Long id;
+    private String productId;
+    private Integer quantity;
+    private BigDecimal price;
+    private BigDecimal subTotal;
+}
