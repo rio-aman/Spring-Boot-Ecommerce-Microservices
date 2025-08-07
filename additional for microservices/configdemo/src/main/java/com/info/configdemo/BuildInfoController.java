e366391c5bb0a544343922414123dcbe020283bd
rio-aman <amprajapati08893.com>
2025-08-07 19:48:07 +0530

    used the configuration properties annotation so then by a one single class with the prefix before of the variable add and makes it easy to fetch the data not just using the value annotation and write the build.id or others everytime

--- additional for microservices/configdemo/src/main/java/com/info/configdemo/BuildInfoController.java
+++ additional for microservices/configdemo/src/main/java/com/info/configdemo/BuildInfoController.java
@@ -1,26 +1,29 @@
 package com.info.configdemo;
 
-import org.springframework.beans.factory.annotation.Value;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class BuildInfoController {
 
-    //    @Value("${OS:default}")
-    @Value("${build.id:default}")
-    private String buildId;
+//    //    @Value("${OS:default}")
+//    @Value("${build.id:default}")
+//    private String buildId;
+//
+//    //    @Value("${PROCESSOR_LEVEL:default}")
+//    @Value("${build.version:default}")
+//    private String buildVersion;
+//
+//    //    @Value("${JAVA_HOME:default}")
+//    @Value("${build.name:default}")
+//    private String buildName;
 
-    //    @Value("${PROCESSOR_LEVEL:default}")
-    @Value("${build.version:default}")
-    private String buildVersion;
-
-    //    @Value("${JAVA_HOME:default}")
-    @Value("${build.name:default}")
-    private String buildName;
+    private BuildInfo buildInfo;
 
     @GetMapping("/build-info")
     public String getBuildInfo() {
-        return "Build ID: " + buildId + ", Version: " + buildVersion + ", Name: " + buildName;
+        return "Build ID: " + buildInfo.getId() + ", Version: " + buildInfo.getVersion() + ", Name: " + buildInfo.getName();
     }
 }
