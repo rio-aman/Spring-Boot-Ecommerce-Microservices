fc86fc2a1087d270c870f0223809f7cffc13511f
rio-aman <amprajapati08893.com>
2025-08-07 10:33:23 +0530

    get to know about the environment variables set for the values in intellij so then it fetch from that and these can done by three ways
    -> BUILD_ID=124;BUILD_VERSION=1.0.0;BUILD_NAME=ThroughIntellijConfiguration
    simply write this
    
    - or using the placeholders and setting these values -> ID=9999;VERSION=1.0.0;NAME=IntellijEnvironmentVariable
    
    and another is through the program command line arguments

--- additional for microservices/configdemo/src/main/java/com/info/configdemo/BuildInfoController.java
+++ additional for microservices/configdemo/src/main/java/com/info/configdemo/BuildInfoController.java
@@ -7,17 +7,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BuildInfoController {
 
-    @Value("${OS:default}")
+    //    @Value("${OS:default}")
+    @Value("${build.id:default}")
     private String buildId;
 
-    @Value("${PROCESSOR_LEVEL:default}")
+    //    @Value("${PROCESSOR_LEVEL:default}")
+    @Value("${build.version:default}")
     private String buildVersion;
 
-    @Value("${JAVA_HOME:default}")
+    //    @Value("${JAVA_HOME:default}")
+    @Value("${build.name:default}")
     private String buildName;
 
     @GetMapping("/build-info")
-    public String getBuildInfo(){
-        return "Build ID: "+buildId+", Version: "+buildVersion+", Name: "+buildName;
+    public String getBuildInfo() {
+        return "Build ID: " + buildId + ", Version: " + buildVersion + ", Name: " + buildName;
     }
 }
