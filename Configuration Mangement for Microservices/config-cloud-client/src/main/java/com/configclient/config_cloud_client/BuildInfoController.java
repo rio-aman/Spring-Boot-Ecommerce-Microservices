ecc5d38dfd425781ae9b4aafb34860b6d0b77423
rio-aman <amprajapati08893.com>
2025-08-08 11:28:46 +0530

    uses the refresh scope annotation concept with the actuator so the refresh the page data without restarting the whole application which reduces the time to see the update in the page using the actuator dependency and @RefreshScope on the BuildInfoController which makes to see the dynamic updates without restarting the whole application

--- Configuration Mangement for Microservices/config-cloud-client/src/main/java/com/configclient/config_cloud_client/BuildInfoController.java
+++ Configuration Mangement for Microservices/config-cloud-client/src/main/java/com/configclient/config_cloud_client/BuildInfoController.java
@@ -1,17 +1,35 @@
 package com.configclient.config_cloud_client;
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@AllArgsConstructor
+//@AllArgsConstructor
+@RefreshScope
 public class BuildInfoController {
 
-    private BuildInfo buildInfo;
+    @Value("${build.id:default}")
+    private String buildId;
+
+    @Value("${build.version:default}")
+    private String buildVersion;
+
+    @Value("${build.name:default}")
+    private String buildName;
 
     @GetMapping("/build-info")
     public String getBuildInfo() {
-        return "Build ID: " + buildInfo.getId() + ", Version: " + buildInfo.getVersion() + ", Name: " + buildInfo.getName();
+        return "Build ID: " + buildId + ", Version: " + buildVersion + ", Name: " + buildName;
     }
+
+//  this when using the BuildInfo class ADDED
+//    private BuildInfo buildInfo;
+
+//    @GetMapping("/build-info")
+//    public String getBuildInfo() {
+//        return "Build ID: " + buildInfo.getId() + ", Version: " + buildInfo.getVersion() + ", Name: " + buildInfo.getName();
+//    }
 }
