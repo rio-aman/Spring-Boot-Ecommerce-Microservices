27c4e2fc4629b2ffb2fc264a6dca3a28b78c6741
rio-aman <amprajapati08893.com>
2025-08-11 14:15:48 +0530

    using Http Interfaces for the inter-service synchronous communication and in this we can config the consumer by these three webclient , rest client or rest template and in HttpInterfaceConfig I have used all the ways and there is a ProviderHttpInterface interface which uses @HttpExchange annotation for the http interface use and the HttpInterfaceController controlling the request for the provider and give the response of it in return

---
+++ Microservices Inter-Service Communication/Synchronous communication using Http Interfaces/consumer_by_http_interfaces/src/main/java/com/demo/consumer/HttpInterfaceConfig.java
@@ -0,0 +1,64 @@
+package com.demo.consumer;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.support.RestTemplateAdapter;
+
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+
+@Configuration
+public class HttpInterfaceConfig {
+
+    // it can be done by using three ways by WebClient, RestClient, or RestTemplate
+
+    //  this when using WEB CLIENT
+//    @Bean
+//    public ProviderHttpInterface webClientHttpInterface(){
+//
+//        WebClient webClient = WebClient.builder()
+//                .baseUrl("http://localhost:8081").build();
+//        WebClientAdapter adapter = WebClientAdapter.create(webClient);
+//        HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
+//
+//        ProviderHttpInterface service = factory.createClient(ProviderHttpInterface.class);
+//
+//        return service;
+//    }
+
+    //  this when using REST CLIENT
+//    @Bean
+//    public ProviderHttpInterface restClientHttpInterface(){
+//
+//        RestClient restClient = RestClient.builder()
+//                .baseUrl("http://localhost:8081").build();
+//        RestClientAdapter adapter = RestClientAdapter.create(restClient);
+//        HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
+//
+//        ProviderHttpInterface service = factory.createClient(ProviderHttpInterface.class);
+//
+//        return service;
+//    }
+
+    //  this when using REST TEMPLATE
+    @Bean
+    public ProviderHttpInterface restTemplateHttpInterface(){
+
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setUriTemplateHandler(new DefaultUriBuilderFactory("http://localhost:8081"));
+        RestTemplateAdapter adapter = RestTemplateAdapter.create(restTemplate);
+        HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
+
+        ProviderHttpInterface service = factory.createClient(ProviderHttpInterface.class);
+
+        return service;
+    }
+}
---
+++ Microservices Inter-Service Communication/Synchronous communication using Http Interfaces/provider_by_http_interfaces/src/main/java/com/demo/provider/HttpInterfacesProviderApplication.java
@@ -0,0 +1,13 @@
+package com.demo.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpInterfacesProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpInterfacesProviderApplication.class, args);
+	}
+
+}
