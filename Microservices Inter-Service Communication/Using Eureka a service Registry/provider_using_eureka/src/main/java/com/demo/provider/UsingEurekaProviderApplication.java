33ee8293f03be2f8e4c72ba8202d1013bd132d6b
rio-aman <amprajapati08893.com>
2025-08-12 12:54:49 +0530

    NOW using the eureka service registry and registered the instances of the provider and consumer microservices by adding some configurations in consumer and provider application properties and also adding the dependency of eureka server registry in both pom.xml

---
+++ Microservices Inter-Service Communication/Using Eureka a service Registry/provider_using_eureka/src/main/java/com/demo/provider/UsingEurekaProviderApplication.java
@@ -0,0 +1,13 @@
+package com.demo.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsingEurekaProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsingEurekaProviderApplication.class, args);
+	}
+
+}
