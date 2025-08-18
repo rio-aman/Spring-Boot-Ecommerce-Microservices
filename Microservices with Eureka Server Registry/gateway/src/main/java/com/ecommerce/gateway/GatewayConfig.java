8b505e6a723b424e557fe8eb5cb9eafe43fb0941
rio-aman <amprajapati08893.com>
2025-08-18 14:22:44 +0530

    rewrite the path of the services so if i want to remove /api then the code written in GatewayConfig and for working it the configuration and bean annotations required

--- Microservices with Eureka Server Registry/gateway/src/main/java/com/ecommerce/gateway/GatewayConfig.java
+++ Microservices with Eureka Server Registry/gateway/src/main/java/com/ecommerce/gateway/GatewayConfig.java
@@ -20,6 +20,26 @@ public class GatewayConfig {
                 .route("order-&-cart-service", r -> r
                         .path("/api/orders/**"," /api/cart/**")
                         .uri("lb://ORDER-CART-SERVICE"))
+
+                    // when want to switch the path ore rewrite the path means removing /api then the below code is written
+
+//                .route("product-service", r -> r
+//                        .path("/products/**")
+//                        .filters(f-> f.rewritePath("/products(?<segment>/?.*)",
+//                                "/api/products${segment}"))
+//                        .uri("lb://PRODUCT-SERVICE"))
+//                .route("user-service", r -> r
+//                        .path("/users/**")
+//                        .filters(f-> f.rewritePath("/users(?<segment>/?.*)",
+//                                "/api/users${segment}"))
+//                        .uri("lb://USER-SERVICE"))
+//                .route("order-cart-service", r -> r
+//                        .path("/orders/**","/cart/**")
+//                        .filters(f-> f.rewritePath("/(?<segment>.*)",
+//                                "/api/${segment}"))
+//                        .uri("lb://ORDER-CART-SERVICE"))
+                // above is the rewrite path of the services
+
                 .route("eureka-server", r -> r
                         .path("/eureka/main")
                         .filters(f -> f.rewritePath("/eureka/main", "/"))
