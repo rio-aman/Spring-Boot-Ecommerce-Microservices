66d9422451ba22fbc4aae0d056296e90dc523d17
rio-aman <amprajapati08893.com>
2025-08-18 23:10:21 +0530

    Now learning about fault tolerance and circuit breaker by Resilience4j

---
+++ Microservices with Eureka , API Gateway and Resilience4j/order/src/main/java/com/ecommerce/order/service/CartService.java
@@ -0,0 +1,72 @@
+package com.ecommerce.order.service;
+
+import com.ecommerce.order.dto.CartItemRequest;
+import com.ecommerce.order.dto.ProductResponse;
+import com.ecommerce.order.dto.UserResponse;
+import com.ecommerce.order.httpinterface.ProductServiceClient;
+import com.ecommerce.order.httpinterface.UserServiceClient;
+import com.ecommerce.order.model.CartItem;
+import com.ecommerce.order.repository.CartItemRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class CartService {
+
+    private final CartItemRepository cartItemRepository;
+    private final ProductServiceClient productServiceClient;
+    private final UserServiceClient userServiceClient;
+
+    public boolean addToCart(String userId, CartItemRequest request) {
+        // look for the product
+        ProductResponse productResponse = productServiceClient.getProductDetails(request.getProductId());
+        if(productResponse == null || productResponse.getStockQuantity() < request.getQuantity())
+            return false;
+
+        UserResponse userResponse = userServiceClient .getUserDetails(userId);
+        if(userResponse == null)
+            return false;
+
+        CartItem existingCartItem = cartItemRepository.findByUserIdAndProductId(userId, request.getProductId());
+        if(existingCartItem != null){
+            //update the quantity
+            existingCartItem.setQuantity(existingCartItem.getQuantity() + request.getQuantity());
+            existingCartItem.setPrice(productResponse.getPrice().multiply(BigDecimal.valueOf(existingCartItem.getQuantity())));
+            cartItemRepository.save(existingCartItem);
+
+        }else {
+            //add the product in user cart as new cart item
+            CartItem cartItem = new CartItem();
+            cartItem.setUserId(userId);
+            cartItem.setProductId(request.getProductId());
+            cartItem.setQuantity(request.getQuantity());
+            cartItem.setPrice(productResponse.getPrice().multiply(BigDecimal.valueOf(request.getQuantity())));
+            cartItemRepository.save(cartItem);
+        }
+        return true;
+    }
+
+    public boolean deleteItemFromCart(String userId, String productId) {
+        CartItem cartItem = cartItemRepository.findByUserIdAndProductId(userId, productId);
+
+        if(cartItem!=null){
+            cartItemRepository.delete(cartItem);
+            return true;
+        }
+        return false;
+    }
+
+    public List<CartItem> getCart(String userId) {
+        return cartItemRepository.findByUserId(userId);
+    }
+
+    public void clearCart(String userId) {
+        cartItemRepository.deleteByUserId(userId);
+    }
+}
---
+++ Microservices with Eureka , API Gateway and Resilience4j/order/src/main/java/com/ecommerce/order/service/OrderService.java
@@ -0,0 +1,83 @@
+package com.ecommerce.order.service;
+
+import com.ecommerce.order.dto.OrderItemDto;
+import com.ecommerce.order.dto.OrderResponse;
+import com.ecommerce.order.dto.UserResponse;
+import com.ecommerce.order.httpinterface.UserServiceClient;
+import com.ecommerce.order.model.*;
+import com.ecommerce.order.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final CartService cartService;
+    private final UserServiceClient userServiceClient;
+    private final OrderRepository orderRepository;
+
+    public Optional<OrderResponse> createOrder(String userId) {
+        //Validate the cart items
+        List<CartItem> cartItems = cartService.getCart(userId);
+        if(cartItems.isEmpty()){
+            return Optional.empty();
+        }
+
+        //Validate for the user
+        UserResponse userResponse = userServiceClient.getUserDetails(userId);
+        if(userResponse == null){
+            return Optional.empty();
+        }
+
+        //Calculate total price
+        BigDecimal totalPrice = cartItems.stream()
+                .map(CartItem::getPrice)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        //Create order
+        Order order = new Order();
+        order.setUserId(userId);
+        order.setStatus(OrderStatus.CONFIRMED);
+        order.setTotalAmount(totalPrice);
+        List<OrderItem> orderItems = cartItems.stream()
+                .map(item -> new OrderItem(
+                        null,
+                        item.getProductId(),
+                        item.getQuantity(),
+                        item.getPrice(),
+                        order
+                ))
+                .toList();
+        order.setItems(orderItems);
+        Order saveOrder = orderRepository.save(order);
+
+        //Clear the cart
+        cartService.clearCart(userId);
+
+        return Optional.of(mapToOrderResponse(saveOrder));
+    }
+
+    private OrderResponse mapToOrderResponse(Order order) {
+        // this method is for converting the type of Order Object into Order Response
+        return new OrderResponse(
+                order.getId(),
+                order.getTotalAmount(),
+                order.getStatus(),
+                order.getItems().stream()
+                        .map(orderItem -> new OrderItemDto(
+                                orderItem.getId(),
+                                orderItem.getProductId(),
+                                orderItem.getQuantity(),
+                                orderItem.getPrice(),
+                                orderItem.getPrice().multiply(new BigDecimal(orderItem.getQuantity()))
+                        ))
+                        .toList(),
+                order.getCreatedAt()
+        );
+    }
+}
