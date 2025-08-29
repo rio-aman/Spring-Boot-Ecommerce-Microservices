635c167b303d643fe30f0cb52f801d65a6c5cbcb
rio-aman <amprajapati08893.com>
2025-08-29 19:55:14 +0530

    Used the spring cloud streams for consumer without writing the configuration for kafka we can interact with the kafka using java configuration and some configuration in xml file

--- Apache Kafka/consumer/src/main/java/com/kafka/consumer/KafkaConsumer.java
+++ Apache Kafka/consumer/src/main/java/com/kafka/consumer/KafkaConsumer.java
@@ -1,26 +1,26 @@
 package com.kafka.consumer;
 
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Component;
-
-@Component
-public class KafkaConsumer {
-
-    @KafkaListener(topics = "my-topic", groupId = "my-new-group")
-    public void listen1(String message){
-        System.out.println("Received Message 1 : " + message);
-    }
-
-//    @KafkaListener(topics = "my-topic", groupId = "my-new-group-1") // this is for multiple consumer working with one request sending
-    @KafkaListener(topics = "my-topic", groupId = "my-new-group")  // if the group id same then the first consumer will listen and rest not
-    public void listen2(String message){
-        System.out.println("Received Message 2 : " + message);
-    }
-
-    @KafkaListener(topics = "my-topic-new", groupId = "my-new-group-rider")
-    public void listenRiderLocation(RiderLocation location){
-        System.out.println("Received Location : " + location.getRiderId() +
-                " : " + location.getLatitude() + " : " + location.getLongitude() );
-    }
-
-}
+//import org.springframework.kafka.annotation.KafkaListener;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class KafkaConsumer {
+//
+//    @KafkaListener(topics = "my-topic", groupId = "my-new-group")
+//    public void listen1(String message){
+//        System.out.println("Received Message 1 : " + message);
+//    }
+//
+////    @KafkaListener(topics = "my-topic", groupId = "my-new-group-1") // this is for multiple consumer working with one request sending
+//    @KafkaListener(topics = "my-topic", groupId = "my-new-group")  // if the group id same then the first consumer will listen and rest not
+//    public void listen2(String message){
+//        System.out.println("Received Message 2 : " + message);
+//    }
+//
+//    @KafkaListener(topics = "my-topic-new", groupId = "my-new-group-rider")
+//    public void listenRiderLocation(RiderLocation location){
+//        System.out.println("Received Location : " + location.getRiderId() +
+//                " : " + location.getLatitude() + " : " + location.getLongitude() );
+//    }
+//
+//}
---
+++ Apache Kafka/consumer/src/main/java/com/kafka/consumer/KafkaConsumerStream.java
@@ -0,0 +1,17 @@
+package com.kafka.consumer;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Consumer;
+
+@Configuration
+public class KafkaConsumerStream {
+
+    @Bean
+    public Consumer<RiderLocation> processRiderLocation() {
+        return location -> {
+            System.out.println("Received : " + location.getRiderId() + " @ " + location.getLatitude() + " , " + location.getLongitude());
+        };
+    }
+}
