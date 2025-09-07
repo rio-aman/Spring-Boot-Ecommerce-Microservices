c86afa24b8d3bca72283dfb319f143a120e259c5
rio-aman <amprajapati08893.com>
2025-09-07 11:41:36 +0530

    added the role based access and created in the keycloak in clients different roles like order, product and user assigned to the users created in user section as order_user, product_user and user respectively and according to the role that can access the api related to that role and others are 403 forbidden (can't access) for this used the Converter for extracting the roles

--- Authentication And Security/Microservices With Keycloak for Security of Data With PKCE FLOW/gateway/src/main/java/com/ecommerce/gateway/security/SecurityConfig.java
+++ Authentication And Security/Microservices With Keycloak for Security of Data With PKCE FLOW/gateway/src/main/java/com/ecommerce/gateway/security/SecurityConfig.java
@@ -2,10 +2,20 @@ package com.ecommerce.gateway.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Map;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -16,9 +26,32 @@ public class SecurityConfig {
         return security
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/api/products/**").hasRole("PRODUCT") // this is for restricting the roles like admin access admin part and user access user part
+                        .pathMatchers("/api/orders/**").hasRole("ORDER")
+                        .pathMatchers("/api/users/**").hasRole("USER")
                         .anyExchange().authenticated())
                 .oauth2ResourceServer(oauth2 ->
-                        oauth2.jwt(Customizer.withDefaults()))
+//                        oauth2.jwt(Customizer.withDefaults())) // before writing the converter
+                        oauth2.jwt(jwt
+                                        -> jwt.jwtAuthenticationConverter(grantedAuthoritiesExtractor())))
                 .build();
+    }
+
+    // this converter is for collecting the roles in Keycloak by the token
+    // this basically extract the user roles from jwt token
+    private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+        ReactiveJwtAuthenticationConverter jwtAuthenticationConverter = new ReactiveJwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            List<String> roles = jwt.getClaimAsMap("resource_access")
+                    .entrySet().stream()
+                    .filter(entry -> entry.getKey().equals("oauth2-pkce"))
+                    .flatMap(entry -> ((Map<String, List<String>>) entry.getValue())
+                            .get("roles").stream())
+                    .toList();
+            System.out.println("Extracted Roles : " + roles);
+            return Flux.fromIterable(roles)
+                    .map(role -> new SimpleGrantedAuthority("ROLE_" + role));
+        });
+        return jwtAuthenticationConverter;
     }
 }
